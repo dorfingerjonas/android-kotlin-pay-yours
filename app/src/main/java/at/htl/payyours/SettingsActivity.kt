cc9817171851a5c2936803405fe9886d4abe749e
Jonas Dorfinger <contact@dorfingerjonas.at>
2021-11-30 14:13:50 +0100

    implement companion object

--- app/src/main/java/at/htl/payyours/SettingsActivity.kt
+++ app/src/main/java/at/htl/payyours/SettingsActivity.kt
@@ -5,6 +5,16 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+    
+    companion object {
+        val LOG_TAG = SettingsActivity::class.java.simpleName
+        
+        val PREFERENCE_FILENAME = "PayYoursReferences"
+        val PRICE_PER_UNIT_KEY = "PRICE_PER_UNIT"
+        val PLAYER_KEY = "DEFAULT_PLAYERS"
+        val COURTS_KEY = "DEFAULTS_COURTS"
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
